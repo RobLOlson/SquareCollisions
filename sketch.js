d17fc78e22ecfc433ff661e5bbe4a28938de57bc
Rob L Olson <rob.louis@gmail.com>
2024-07-19 19:03:09 -0800

    fix some physics (and break others)

--- sketch.js
+++ sketch.js
@@ -10,8 +10,11 @@ let PREV_MOUSE_Y = 0;
 let DEBUG_TEXT = false;
 let DEBUG = false;
 let SKIP = false;
-let SIZE = 1;
+let SIZE = 100;
 let BALL_COUNT = 0;
+let BLOCK_FILL = "green"
+let PAUSE_FRAME = false
+let HOLD_BALL = false
 
 class Point{
     constructor(x, y, size, color) {
@@ -53,6 +56,7 @@ class Line{
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+        LINES.push(this)
     }
     update(){
     }
@@ -84,6 +88,11 @@ class Block{
         this.velocity = createVector(0,0);
         // this.velocity.setMag(random(0.5,1.5));
         this.acceleration = createVector(0,0);
+
+        this.corner_top_left = createVector(this.position.x + min(this.corner, this.width/2, this.depth/2), this.position.y + min(this.corner, this.depth/2, this.width/2))
+        this.corner_top_right = createVector(this.position.x - min(this.corner, this.width/2, this.depth/2) + this.width, this.position.y + min(this.corner, this.depth/2, this.width/2))
+        this.corner_bot_left = createVector(this.position.x + min(this.corner, this.width/2, this.depth/2), this.position.y + this.depth - min(this.corner, this.depth/2, this.width/2))
+        this.corner_bot_right = createVector(this.position.x + this.width - min(this.corner, this.width/2, this.depth/2), this.position.y + this.depth - min(this.corner, this.depth/2, this.width/2))
     }
 
     update() {
@@ -93,8 +102,18 @@ class Block{
 
     show() {
         stroke(255);
-        fill("green");
+        fill(BLOCK_FILL);
         rect(this.x, this.y, this.width, this.depth, this.corner, this.corner, this.corner, this.corner);
+        circle(this.corner_top_left.x, this.corner_top_left.y, 5)
+        circle(this.corner_top_right.x, this.corner_top_right.y, 5)
+        circle(this.corner_bot_left.x, this.corner_bot_left.y, 5)
+        circle(this.corner_bot_right.x, this.corner_bot_right.y, 5)
+
+        circle(this.corner_top_left.x, this.corner_top_left.y, min(this.width/2, this.depth/2, this.corner))
+        line(this.corner_top_left.x, this.corner_top_left.y, this.corner_top_left.x, this.y)
+
+
+
         fill("white");
     }
 }
@@ -109,11 +128,18 @@ class Ball{
         this.position = createVector(x, y);
         this.acceleration = createVector(0, .1);
         this.size = size;
+        this.skip_frame = false
         this.id = BALL_COUNT;
+        BALLS.push(this)
         BALL_COUNT++;
     }
 
     update() {
+
+        if(this.skip_frame){
+            this.skip_frame = false
+            return
+        }
 
         if(SKIP > 0){
 
@@ -132,21 +158,21 @@ class Ball{
 
         // WALL COLLISION
         // VVVVVVVVVVVVVV
-        if(this.position.x - this.size / 2 < 0){
-            this.position.x = this.size / 2;
+        if(this.position.x - this.size < 0){
+            this.position.x = this.size;
             this.velocity.x = this.velocity.x * -1;
         }
-        if(this.position.x + this.size / 2 > C_W) {
-            this.position.x = C_W-this.size/2;
+        if(this.position.x + this.size > C_W) {
+            this.position.x = C_W-this.size;
             this.velocity.x = this.velocity.x * -1;
         }
 
-        if(this.position.y -this.size / 2 < 0){
-            this.position.y = this.size/2;
+        if(this.position.y -this.size < 0){
+            this.position.y = this.size;
             this.velocity.y = this.velocity.y * -1;
         }
-        if(this.position.y + this.size / 2 > C_H){
-            this.position.y = C_H-this.size/2;
+        if(this.position.y + this.size > C_H){
+            this.position.y = C_H-this.size;
             this.velocity.y = this.velocity.y * -1;
         }
 
@@ -165,52 +191,181 @@ class Ball{
             // All distances are positive if "outside" the block and negative if "inside"
 
             // `top` is the ball's (signed) distance from the top of the block
-            let top = block.position.y - next.y;
+            let top_n = block.position.y - next.y;
 
             // `bottom` is the ball's (signed) distance from the bottom of the block
-            let bottom = next.y - (block.position.y + block.depth);
+            let bottom_n = next.y - (block.position.y + block.depth);
 
             // `left` is the ball's (signed) distance from the left side of the block
-            let left = block.position.x - next.x;
+            let left_n = block.position.x - next.x;
 
             // `right` is the ball's (signed) distance from the right side of the block
-            let right = next.x - (block.position.x + block.width);
+            let right_n = next.x - (block.position.x + block.width);
+
+            let corner_contact_distance = this.size + block.corner
 
             // if shortest distance is less than radius of ball, possible collision
-            // if(choice < this.size/2){
-            if(left < this.size/2 && right < this.size/2 && top < this.size/2 && bottom < this.size/2){
-                // recalculate distances from CURRENT position
-                top = block.position.y + block.corner - this.position.y;
-                bottom = this.position.y - (block.position.y + block.depth - block.corner);
-                left = block.position.x + block.corner - this.position.x;
-                right = this.position.x - (block.position.x + block.width - block.corner);
+            // if(choice < this.size){
+            if(FLAG){
+                debugger
+            }
+            if(left_n < this.size && right_n < this.size && top_n < this.size && bottom_n < this.size){
+                BLOCK_FILL = "yellow"
+                // let corner_top_left = createVector(block.position.x - block.corner, block.position.y - block.corner)
+                // let corner_top_right = createVector(block.position.x + block.width - block.corner, block.position.y - block.corner)
+                // let block.corner_bot_left = createVector(block.position.x, block.position.y - block.corner + block.depth - block.corner)
+                // let block.corner_bot_right = createVector(block.position.x + block.width - block.corner, block.position.y + block.depth - block.corner)
 
-                if(top > 0 && bottom < 0 && left < 0 && right < 0){ // top side collision
-                    this.velocity.y *= -1;
-                } else
-                if(bottom > 0 && top < 0 && left < 0 && right < 0){ // bottom side collision
-                    this.velocity.y *= -1;
-                } else
-                if(left > 0 && right < 0 && top < 0 && bottom < 0){ // left side collision
-                    this.velocity.x *= -1;
-                } else
-                if(right > 0 && left < 0 && top < 0 && bottom < 0){ // right side collision
-                    this.velocity.x *= -1;
-                } else
-                if(top > 0 && bottom < 0 && left > 0 && right < 0){ // top left corner collision
-                    this.velocity = this.corner_collide(block.position.x+block.corner, block.position.y+block.corner);
-                } else
-                if(top > 0 && bottom < 0 && right > 0 && left < 0){ // top right corner collision
-                    this.velocity = this.corner_collide(block.position.x+block.width-block.corner, block.position.y+block.corner);
-                } else
-                if(bottom > 0 && top < 0 && left > 0 && right < 0){ // bottom left corner collision
-                    this.velocity = this.corner_collide(block.position.x+block.corner, block.position.y+block.depth-block.corner);
-                } else
-                if(bottom > 0 && top < 0 && right > 0 && left < 0){
-                    this.velocity = this.corner_collide(block.position.x+block.width-block.corner, block.position.y+block.depth-block.corner);
-                } else {
-                    noLoop();
+                // recalculate distances from CURRENT position
+                let top = block.position.y - this.position.y;
+                let bottom = this.position.y - (block.position.y + block.depth);
+                let left = block.position.x - this.position.x;
+                let right = this.position.x - (block.position.x + block.width);
+
+                let d_top_left = this.position.dist(block.corner_top_left)
+                let d_top_right = this.position.dist(block.corner_top_right)
+                let d_bot_left = this.position.dist(block.corner_bot_left)
+                let d_bot_right = this.position.dist(block.corner_bot_right)
+
+                let closest_corner = min([d_top_left, d_top_right, d_bot_left, d_bot_right])
+                // let closest_edge = min([abs(top), abs(bottom), abs(left), abs(right)])
+                let closest_edge = max([top, bottom, left, right])
+
+
+                if(closest_corner < corner_contact_distance){
+                    // delta_s is the fraction of a frame/time-step that results in perfect contact
+                    let delta_s = (corner_contact_distance - closest_corner) / corner_contact_distance
+                    this.position.x += this.velocity.x * delta_s
+                    this.position.y += this.velocity.y * delta_s
+                    // if(closest_corner == d_top_left && top > -1*(block.corner+this.velocity.mag()) && left > -1*(block.corner+this.velocity.mag())){
+                    if(closest_corner == d_top_left && this.position.x < block.corner_top_left.x && this.position.y < block.corner_top_left.y){
+                        BLOCK_FILL = "red"
+                        debugger
+                        this.velocity = this.corner_collide(block.corner_top_left)
+                        this.position.x += this.velocity.x * (1-delta_s)
+                        this.position.y += this.velocity.y * (1-delta_s)
+                        this.skip_frame = true
+                        continue
+                    }
+                    // else if (closest_corner == d_top_right && top > -1*(block.corner+this.velocity.mag())  && right > -1*(block.corner+this.velocity.mag())) {
+                    else if(closest_corner == d_top_right && this.position.x > block.corner_top_right.x && this.position.y < block.corner_top_right.y){
+
+                        this.velocity = this.corner_collide(block.corner_top_right)
+                        this.position.x += this.velocity.x * (1-delta_s)
+                        this.position.y += this.velocity.y * (1-delta_s)
+                        this.skip_frame = true
+                        continue
+                    }
+                    // else if (closest_corner == d_bot_left && bottom > -1*(block.corner+this.velocity.mag()) && left > -1*(block.corner+this.velocity.mag())) {
+                    if(closest_corner == d_bot_left && this.position.x < block.corner_bot_left.x && this.position.y > block.corner_bot_left.y){
+                        this.velocity = this.corner_collide(block.corner_bot_left)
+                        this.position.x += this.velocity.x * (1-delta_s)
+                        this.position.y += this.velocity.y * (1-delta_s)
+                        this.skip_frame = true
+                        continue
+                    }
+                    // else if (closest_corner == d_bot_right && bottom > -1*(block.corner+this.velocity.mag()) && right > -1*(block.corner+this.velocity.mag())) {
+                    if(closest_corner == d_bot_right && this.position.x > block.corner_bot_right.x && this.position.y > block.corner_bot_right.y){
+                        this.velocity = this.corner_collide(block.corner_bot_right)
+                        this.position.x += this.velocity.x * (1-delta_s)
+                        this.position.y += this.velocity.y * (1-delta_s)
+                        this.skip_frame = true
+                        continue
+                    }
+                    this.position.x -= this.velocity.x * delta_s
+                    this.position.y -= this.velocity.y * delta_s
                 }
+                if(closest_edge < this.size){
+                    if((closest_edge==top && left < -block.corner && right < -block.corner) || top * top_n < 1){
+                        this.velocity.y *= -1
+                        this.position.add(this.velocity)
+                        this.skip_frame=true
+                    } else
+                    if((closest_edge==bottom && left < -block.corner && right < -block.corner) || bottom * bottom_n < 1){
+                        this.velocity.y *= -1
+                        this.position.add(this.velocity)
+                        this.skip_frame=true
+                    } else
+                    if((closest_edge==left && top < -block.corner && bottom < -block.corner) || left*left_n < 0){
+                        this.velocity.x *= -1
+                        this.position.add(this.velocity)
+                        this.skip_frame=true
+                    } else
+                    if((closest_edge==right && top < -block.corner && bottom < -block.corner) || right*right_n < 0){
+                        this.velocity.x *= -1
+                        this.position.add(this.velocity)
+                        this.skip_frame=true
+                    } else
+                    {
+                        BLOCK_FILL = "green"
+                        // debugger
+                    }
+                }
+                // else
+                // {
+                //     console.log(`top=${top}\nbottom=${bottom}\nleft=${left}\nright=${right}`)
+                //     console.log(`top_n=${top_n}\nbottom_n=${bottom_n}\nleft_n=${left_n}\nright_n=${right_n}`)
+                //     console.log(`closest_edge=${closest_edge}`)
+                //     console.log(`closest_corner=${closest_corner}`)
+                //     console.log(`dist(block.corner_top_left)=${next.dist(block.corner_top_left)}`)
+                //     console.log(`dist(block.corner_top_right)=${next.dist(block.corner_top_right)}`)
+                //     console.log(`dist(block.corner_bot_left)=${next.dist(block.corner_bot_left)}`)
+                //     console.log(`dist(block.corner_bot_right)=${next.dist(block.corner_bot_right)}`)
+                //     console.log(`corner_contact_distance=${corner_contact_distance}`)
+                //     debugger
+                //     noLoop()
+                // }
+
+
+                //DEBUGGING STUFF
+                // if(top < this.size && bottom < this.size && left < this.size && right < this.size){
+                //     BLOCK_FILL = "red"
+                // } else {
+                //     BLOCK_FILL = "green"
+                // }
+
+                // if(top > 0 && bottom < 0 && left > 0 && right < 0 && next.dist(corner_top_left) < this.size + block.corner){ // top left corner collision
+                //     // this.velocity = this.corner_collide(block.position.x+block.corner, block.position.y+block.corner);
+                //     this.velocity = this.corner_collide(corner_top_left);
+                //     console.log(`${next.dist(corner_top_left)}`)
+                // } else
+                // if(top > 0 && bottom < 0 && right > 0 && left < 0 && next.dist(corner_top_right) < this.size + block.corner){ // top right corner collision
+                //     this.velocity = this.corner_collide(corner_top_right);
+                // } else
+                // if(bottom > 0 && top < 0 && left > 0 && right < 0 && next.dist(corner_bot_left) < this.size + block.corner){ // bottom left corner collision
+                //     this.velocity = this.corner_collide(corner_bot_left);
+                // } else
+                // if(bottom > 0 && top < 0 && right > 0 && left < 0 && next.dist(corner_bot_right) < this.size + block.corner){
+                //     this.velocity = this.corner_collide(corner_bot_right);
+                // }
+                // else
+                // if(top > 0 && bottom < 0 && left < 0 && right < 0){ // top side collision
+
+                //     this.velocity.y *= -1;
+                // } else
+                // if(bottom > 0 && top < 0 && left < 0 && right < 0){ // bottom side collision
+                //     this.velocity.y *= -1;
+                // } else
+                // if(left > 0 && right < 0 && top < 0 && bottom < 0){ // left side collision
+                //     this.velocity.x *= -1;
+                // } else
+                // if(right > 0 && left < 0 && top < 0 && bottom < 0){ // right side collision
+                //     this.velocity.x *= -1;
+                // }
+
+                // else {
+                //     noLoop();
+                //     console.log(`top=${top}\nbottom=${bottom}\nleft=${left}\nright=${right}`)
+                //     console.log(`top_n=${top_n}\nbottom_n=${bottom_n}\nleft_n=${left_n}\nright_n=${right_n}`)
+                //     console.log(`dist(corner_top_left)=${next.dist(corner_top_left)}`)
+                //     console.log(`dist(corner_top_right)=${next.dist(corner_top_right)}`)
+                //     console.log(`dist(corner_bot_left)=${next.dist(corner_bot_left)}`)
+                //     console.log(`dist(corner_bot_right)=${next.dist(corner_bot_right)}`)
+
+                //     console.log(`ball.size/2 + block.corner=${this.size + block.corner}`)
+
+
+                // }
             }
         }
 
@@ -228,9 +383,10 @@ class Ball{
 
     }
 
-    corner_collide(corner_x, corner_y){
+    // corner_collide(corner_x, corner_y){
+    corner_collide(corner){
         let start = this.velocity.mag()
-        let corner = createVector(corner_x, corner_y);
+        // let corner = createVector(corner_x, corner_y);
         let normal = createVector(this.position.x, this.position.y).sub(corner);
         let nTn = normal.dot(normal);
         let nTv = normal.dot(this.velocity);
@@ -250,9 +406,12 @@ class Ball{
 
 
 function setup() {
+    ellipseMode(RADIUS)
     createCanvas(C_W, C_H);
-    frameRate(120);
-    BALLS.push(new Ball(200, 200, 15, createVector(random(1,5), random(1,5))));
+    frameRate(60);
+    // BALLS.push(new Ball(200, 200, 15, createVector(random(1,5), random(1,5))));
+    new Ball(200, 200, 15, createVector(random(1,5), random(1,5)));
+
 
     if(DEBUG){
         for(i=0; i<5; i++){
@@ -261,10 +420,18 @@ function setup() {
     }
     strokeWeight(1);
 }
+let FLAG = false
 
 function keyTyped(){
     if(keyCode===32){
-        BALLS.push(new Ball(10, 10, 15, createVector(random(1,5), random(1,5))));
+        new Ball(10, 10, 15, createVector(random(1,5), random(1,5)))
+    }
+    if(keyCode==113){
+        FLAG = true
+    }
+    if(keyCode==104)
+    {
+        HOLD_BALL = !HOLD_BALL
     }
 }
 
@@ -279,13 +446,16 @@ function mouseReleased(){
 
     loop();
 
-    let left = min(mouseX, PREV_MOUSE_X);
-    let top = min(mouseY, PREV_MOUSE_Y);
-    let width = abs(mouseX - PREV_MOUSE_X);
-    let depth = abs(mouseY - PREV_MOUSE_Y);
+    if(mouseButton == "left") {
 
-    if(!DEBUG){
-        BLOCKS.push(new Block(left, top, width, depth));
+        let left = min(mouseX, PREV_MOUSE_X);
+        let top = min(mouseY, PREV_MOUSE_Y);
+        let width = abs(mouseX - PREV_MOUSE_X);
+        let depth = abs(mouseY - PREV_MOUSE_Y);
+
+        if(!DEBUG){
+            BLOCKS.push(new Block(left, top, width, depth));
+        }
     }
 
 }
@@ -296,6 +466,13 @@ function draw() {
   for (let block of BLOCKS){
     block.update();
     block.show();
+  }
+
+  if(HOLD_BALL){
+      BALLS[0].velocity.x=0
+      BALLS[0].velocity.y=0
+      BALLS[0].position.x=mouseX
+      BALLS[0].position.y=mouseY
   }
 
   for (let ball of BALLS){
